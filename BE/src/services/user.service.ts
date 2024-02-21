f386411845e511b2c35351fb3977be138d758d06
Niv Greenstein <greenstein9@gmail.com>
2024-02-21 23:34:25 +0200

    feat: created user service and controller

--- BE/src/services/user.service.ts
+++ BE/src/services/user.service.ts
@@ -1,16 +1,61 @@
+import { ObjectId, WithId } from 'mongodb';
+import bcrypt from 'bcrypt';
 import { getDb } from '../DB/monogDb';
-import { ObjectId } from 'mongodb';
 import { User } from '../models/user.model';
+import { PartialUserUpdate, UserWithPassword } from '../controllers/user.controller';
 
 const getCollection = async () => {
-	const db = await getDb();
-	return db.collection('users');
+  const db = await getDb();
+  return db.collection<User & { password: string }>('users');
 };
 
-export const updateUser = async (user: User) => {
-	const collection = await getCollection();
-	return await collection.updateOne(
-		{ _id: new ObjectId(user._id) },
-		{ $set: { ...user } }
-	);
+export const updateUser = async (user: PartialUserUpdate) => {
+  const collection = await getCollection();
+  const updatedUser = { ...user, _id: undefined, updatedAt: new Date() };
+  delete updatedUser._id;
+  return await collection.updateOne({ _id: new ObjectId(user._id) }, { $set: { ...updatedUser } });
+};
+
+export const getUserById = async (id: string) => {
+  const collection = await getCollection();
+  const user: WithId<User> | null = await collection.findOne({ _id: new ObjectId(id) }, { projection: { password: 0 } });
+  return user;
+};
+
+export const getUserByEmail = async (email: string) => {
+  const collection = await getCollection();
+  return await collection.findOne({ email }, { projection: { password: 0 } });
+};
+
+export const createUser = async (user: UserWithPassword) => {
+  const collection = await getCollection();
+
+  const exsits = await collection.findOne(
+    {
+      $or: [{ username: user.username }, { email: user.email }],
+    },
+    { projection: { password: 0 } }
+  );
+
+  if (exsits) {
+    throw new Error('User already exists');
+  }
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+
+  const result = await collection.insertOne({
+    ...user,
+    password: hashedPassword,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  if (!result.acknowledged) {
+    throw new Error('Could not create user');
+  }
+  return result.insertedId;
+};
+
+export const deleteUser = async (id: string) => {
+  const collection = await getCollection();
+  return await collection.deleteOne({ _id: new ObjectId(id) });
 };
