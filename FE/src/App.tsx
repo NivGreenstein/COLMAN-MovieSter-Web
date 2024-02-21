e76f7c5bc734893abe277647da126180cf740a8a
guylevi <guylevi79@gmail.com>
2024-02-21 23:48:01 +0200

    added google login button

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/LoginPage/LoginPage'; // Adjust the import path as necessary
+import LoginPage from './components/auth/LoginPage/LoginPage'; // Adjust the import path as necessary
 
 const App: React.FC = () => {
     return (
--- FE/src/components/auth/LoginPage/LoginPage.tsx
+++ FE/src/components/auth/LoginPage/LoginPage.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
+import GoogleLoginButton from "../GoogleLoginButton/GoogleLoginButton";
+import logo from "../../../assets/logo.png"; // Adjust the path accordingly
 
 
 const LoginPage: React.FC = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [emailValid, setEmailValid] = useState<boolean>(true);
+    const [passwordValid, setPasswordValid] = useState<boolean>(true)
 
     const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('Login with:', email, password);
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+        setEmailValid(emailRegex.test(email));
+        setPasswordValid(passwordRegex.test(password));
+
+        if(emailRegex.test(email) && passwordRegex.test(password)) {
+            console.log('Login with:', email, password);
+        }
     };
 
     return (
         <div className="login-container">
             <form onSubmit={handleLogin}>
-                <img src="../../../public/logo.png" alt="App Logo" className="app-logo" />
+                <img src={logo} alt="App Logo" className="app-logo" />
                 <div>
                     <input
                         type="email"
@@ -22,8 +34,10 @@ const LoginPage: React.FC = () => {
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="Email"
-                        className="email-input"
+                        className={`input-field ${!emailValid ? 'invalid' : ''}`}
                     />
+                    {!emailValid && <p className="error-message">Please enter a valid email address.</p>}
+
                 </div>
                 <div>
                     <input
@@ -32,9 +46,11 @@ const LoginPage: React.FC = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="Password"
-                        className="password-input"
+                        className={`input-field ${!passwordValid ? 'invalid' : ''}`}
                     />
+                    {!passwordValid && <p className="error-message">Password must contain letters and numbers.</p>}
                 </div>
+                <GoogleLoginButton></GoogleLoginButton>
                 <button className="login-button" type="submit">Login</button>
                 <p className="signup-text">Don't have an account? Sign up.</p>
             </form>
