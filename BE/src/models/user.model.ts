cfa3c044142169221465d5acb00e2175a3d8ac57
Niv Greenstein <greenstein9@gmail.com>
2024-02-21 22:17:53 +0200

    eslint and tsconfig fix

--- BE/src/models/user.model.ts
+++ BE/src/models/user.model.ts
@@ -2,12 +2,12 @@ import { ObjectId } from 'mongodb';
 import { z } from 'zod';
 
 export const UserSchema = z.object({
-	_id: z.string().optional(),
-	username: z.string(),
-	email: z.string().email(),
-	profilePicture: z.string().url().optional(),
-	createdAt: z.date().optional(),
-	updatedAt: z.date().optional(),
+  _id: z.instanceof(ObjectId).optional(),
+  username: z.string(),
+  email: z.string().email(),
+  profilePicture: z.string().url().optional(),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
 });
 
 export type User = z.infer<typeof UserSchema>;
