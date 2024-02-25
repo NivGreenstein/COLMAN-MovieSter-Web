bbe3eaac7c1fd028081558b6f82c13ea29b74b24
Niv Greenstein <greenstein9@gmail.com>
2024-02-25 20:27:40 +0200

    feat: added comment router to main router

--- BE/src/routes/index.ts
+++ BE/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import userRouter from './user.router';
 import movieRouter from './movie.router';
+import commentRouter from './comment.router';
 
 const router = Router();
 router.use('/users', userRouter);
 router.use('/movies', movieRouter);
+router.use('/comments', commentRouter);
 
 export default router;
