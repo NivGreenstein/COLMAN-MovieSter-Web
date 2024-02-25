1d7aa855d93ec8be8790222fe39dce62a3e71713
Niv Greenstein <greenstein9@gmail.com>
2024-02-25 22:15:48 +0200

    feat: added cookie parser

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -3,6 +3,7 @@ import { config as dotenvConfig } from 'dotenv';
 import helmet from 'helmet';
 //@ts-ignore
 import { xss } from 'express-xss-sanitizer';
+import cookieParser from 'cookie-parser';
 import mainRouter from './routes';
 import cors, { CorsOptions } from 'cors';
 import { closeMongoDbConnection, getMongoDbClient } from './DB/monogDb';
@@ -11,20 +12,20 @@ dotenvConfig();
 const { PORT: serverPort } = process.env;
 const app = express();
 const corsOptions: CorsOptions = {
-	origin: ['http://localhost:3000', 'http://localhost:4200'],
+  origin: ['http://localhost:3000', 'http://localhost:4200'],
 };
 
-app.use(express.json({}), helmet(), xss(), cors(corsOptions));
+app.use(express.json({}), helmet(), xss(), cors(corsOptions), cookieParser());
 app.use('/', mainRouter);
 
 app.listen(serverPort, async () => {
-	await getMongoDbClient();
-	console.log(`server is listening on port ${serverPort}`);
+  await getMongoDbClient();
+  console.log(`server is listening on port ${serverPort}`);
 });
 
 const shutdown = async () => {
-	console.log('Gracefully shutting down');
-	await closeMongoDbConnection();
+  console.log('Gracefully shutting down');
+  await closeMongoDbConnection();
 };
 
 process.on('SIGTERM', shutdown);
