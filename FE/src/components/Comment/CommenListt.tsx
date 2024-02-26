427cba0a51e26b50e2fef8df3c0ecf6e47bb242e
guylevi <guylevi79@gmail.com>
2024-02-26 17:30:33 +0200

    added style

--- FE/src/components/Comment/CommenListt.tsx
+++ FE/src/components/Comment/CommenListt.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Tooltip, List, Rate } from 'antd';
-import { IUserComment, IMovieComment } from '../../types/IComment'; // Assuming this is the correct path
+import {Tooltip, List, Rate} from 'antd';
+import {IUserComment, IMovieComment} from '../../types/IComment'; // Assuming this is the correct path
 import {Comment as AntComment} from "@ant-design/compatible";
 import moment from 'moment';
 
-// Assuming you have defined and exported IUserComment and IMovieComment interfaces correctly in your types file
 interface CommentListProps {
     comments: (IUserComment | IMovieComment)[];
     isMoviePage: boolean;
 }
 
-const CommentList: React.FC<CommentListProps> = ({ comments, isMoviePage }) => {
+const CommentList: React.FC<CommentListProps> = ({comments, isMoviePage}) => {
     return (
         <List
             className="comment-list"
-            header={`${comments.length} ${comments.length > 1 ? 'comments' : 'comment'}`}
             itemLayout="horizontal"
             dataSource={comments}
             renderItem={(comment: IUserComment | IMovieComment) => (
@@ -23,7 +21,7 @@ const CommentList: React.FC<CommentListProps> = ({ comments, isMoviePage }) => {
                         author={isMoviePage ? (comment as IMovieComment).username : (comment as IUserComment).movieTitle}
                         content={
                             <>
-                                <Rate disabled defaultValue={comment.rating} />
+                                <Rate disabled defaultValue={comment.rating}/>
                                 <p>{comment.text}</p>
                             </>
                         }
