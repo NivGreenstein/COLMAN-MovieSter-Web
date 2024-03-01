2232b194483ea489e8d753522d183c26ef3560c8
Niv Greenstein <greenstein9@gmail.com>
2024-03-01 14:39:09 +0200

    deleted unused file

--- BE/src/routes/helloWorld.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { helloWorld } from '../controllers/helloWorld.controller';
-
-const router = Router();
-
-router.post('/', helloWorld);
-
-export default router;
