bf51a72ca89480ea4f60388b0aa6bc1565bd8a5b
Niv Greenstein <greenstein9@gmail.com>
2024-03-01 18:52:02 +0200

    fix: fixed movie info page

--- FE/src/components/MovieInfo/MovieInfPage.tsx
+++ FE/src/components/MovieInfo/MovieInfPage.tsx
@@ -1,46 +1,48 @@
-import React, {useEffect, useState} from 'react';
-import {Layout, Row, Col, Rate, Typography, Divider} from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Layout, Row, Col, Rate, Typography, Divider } from 'antd';
 import CommentList from '../Comments/CommentsList';
-import {Comment} from '../../types/IComment';
-import {IMovie} from '../../types/IMovie';
-import {useParams} from "react-router-dom";
-import {fetchMovieById} from "../../services/movies.service";
+import { Comment } from '../../types/IComment';
+import { IMovie } from '../../types/IMovie';
+import { useParams } from 'react-router-dom';
+import { fetchMovieById } from '../../services/movies.service';
+import { getCommentsByMovieId } from '../../services/comments.service';
 
-
-const {Content} = Layout;
-const {Title} = Typography;
-
+const { Content } = Layout;
+const { Title, Paragraph } = Typography;
 
 const MovieInfoPage: React.FC = () => {
-    const { id } = useParams();
-    const [movie, setMovie] = useState<IMovie | null>(null);
+  const { id } = useParams();
+  const [movie, setMovie] = useState<IMovie | null>(null);
+  const [comments, setComments] = useState<Comment[]>([]);
 
-    useEffect(() => {
-        if (id) {
-            fetchMovieById(id).then(setMovie);
-        }
-    }, [id]);
-
-    if (!movie) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    if (id) {
+      fetchMovieById(id).then((data) => setMovie(data));
+      getCommentsByMovieId(id).then((data) => setComments(data ?? []));
     }
-    return (
-        <Layout>
-            <Content style={{padding: '2rem'}}>
-                <Row gutter={16}>
-                    <Col span={8}>
-                        <img src={movie.imageUrl} alt={movie.title} style={{width: '100%', height: 'auto'}}/>
-                    </Col>
-                    <Col span={16}>
-                        <Title level={2}>{movie.title}</Title>
-                        <Rate disabled defaultValue={5}/> {/* Replace `5` with `movie.rating` from your data */}
-                        <Divider/>
-                        <CommentList comments={comments} isMoviePage={true}/>
-                    </Col>
-                </Row>
-            </Content>
-        </Layout>
-    );
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Layout>
+      <Content style={{ padding: '2rem' }}>
+        <Row gutter={16}>
+          <Col span={8}>
+            <img src={movie.posterUrl} alt={movie.title} style={{ width: '100%', height: 'auto' }} />
+          </Col>
+          <Col span={16}>
+            <Title level={2}>{movie.title}</Title>
+            <Rate value={movie.rating / 2} />
+            <Paragraph>{movie.description}</Paragraph>
+            <Divider />
+            <CommentList comments={comments} isMoviePage={true} />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
 };
 
 export default MovieInfoPage;
