755b433f45c66e1f2ded54b0f7a1a8071d272057
Niv Greenstein <greenstein9@gmail.com>
2024-03-01 18:52:24 +0200

    fix: change comment schema to zod

--- FE/src/types/IComment.ts
+++ FE/src/types/IComment.ts
@@ -1,16 +1,35 @@
-export interface IUserComment {
-    movieTitle: string;
-    rating: number;
-    text: string;
-    date: string | Date;
-}
+// export interface IUserComment {
+//     movieTitle: string;
+//     rating: number;
+//     text: string;
+//     date: string | Date;
+// }
 
-export interface IMovieComment {
-    username: string;
-    rating: number;
-    text: string;
-    date: string | Date;
-}
+// export interface IMovieComment {
+//     username: string;
+//     rating: number;
+//     text: string;
+//     date: string | Date;
+// }
 
+// export type Comment = IUserComment | IMovieComment;
 
-export type Comment = IUserComment | IMovieComment;
+import { z } from 'zod';
+
+export const CommentSchema = z
+  .object({
+    _id: z.string().regex(/^[0-9a-fA-F]{24}$/),
+    movieId: z.number(),
+    userId: z.string().regex(/^[0-9a-fA-F]{24}$/),
+    mainCommentId: z
+      .string()
+      .regex(/^[0-9a-fA-F]{24}$/)
+      .optional(),
+    description: z.string(),
+    rating: z.number().min(0).max(5),
+    createdAt: z.date(),
+    updatedAt: z.date(),
+  })
+  .strict();
+
+export type Comment = z.infer<typeof CommentSchema>;
