e136778956a2f056a69929d62f840e893ba32aaf
Niv Greenstein <greenstein9@gmail.com>
2024-03-01 18:53:26 +0200

    fix: changed handleSearch to be triggered onChange instead of onSearch

--- FE/src/components/header/AppHeader.tsx
+++ FE/src/components/header/AppHeader.tsx
@@ -8,33 +8,33 @@ const { Header } = Layout;
 const { Search } = Input;
 
 interface AppHeaderProps {
-    handleLogout: () => void;
-    handleSearch: (value: string) => void;
-    goToProfile: () => void;
-    profileImageUrl?: string;
+  handleLogout: () => void;
+  handleSearch: (value: string) => void;
+  goToProfile: () => void;
+  profileImageUrl?: string;
 }
 
 const AppHeader: React.FC<AppHeaderProps> = ({ handleLogout, handleSearch, goToProfile, profileImageUrl }) => {
-    return (
-        <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 50px' }}>
-            <Button type="primary" onClick={handleLogout}>
-                Log out
-            </Button>
-            <Search
-                placeholder="Search movies"
-                onSearch={handleSearch}
-                enterButton
-                size="large"
-                style={{
-                    borderRadius: '25px',
-                    overflow: 'hidden',
-                    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-                    width: '600px'
-                }}
-            />
-            <Avatar size="large" onClick={goToProfile} icon={<UserOutlined />} src={profileImageUrl} />
-        </Header>
-    );
+  return (
+    <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 50px' }}>
+      <Button type="primary" onClick={handleLogout}>
+        Log out
+      </Button>
+      <Search
+        placeholder="Search movies"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
+        enterButton
+        size="large"
+        style={{
+          borderRadius: '25px',
+          overflow: 'hidden',
+          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+          width: '600px',
+        }}
+      />
+      <Avatar size="large" onClick={goToProfile} icon={<UserOutlined />} src={profileImageUrl} />
+    </Header>
+  );
 };
 
 export default AppHeader;
