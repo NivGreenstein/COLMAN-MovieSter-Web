d1458d4d4c66b73562c03d19e4d8ec6237d1260d
guylevi <guylevi79@gmail.com>
2024-03-01 17:40:58 +0200

    deleted props of app header

--- FE/src/components/header/AppHeader.tsx
+++ FE/src/components/header/AppHeader.tsx
@@ -1,25 +1,37 @@
-// src/components/AppHeader.tsx
-
 import React from 'react';
-import { Layout, Input, Avatar, Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import {Layout, Input, Avatar, Button} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import {useNavigate} from 'react-router-dom';
+import {logout} from '../../services/auth.service';
 
-const { Header } = Layout;
-const { Search } = Input;
+const {Header} = Layout;
+const {Search} = Input;
 
-interface AppHeaderProps {
-    handleLogout: () => void;
-    handleSearch: (value: string) => void;
-    goToProfile: () => void;
-    profileImageUrl?: string;
-}
+const AppHeader: React.FC<{ profileImageUrl?: string }> = ({profileImageUrl}) => {
+    const navigate = useNavigate();
 
-const AppHeader: React.FC<AppHeaderProps> = ({ handleLogout, handleSearch, goToProfile, profileImageUrl }) => {
+    const handleLogout = async () => {
+        try {
+            await logout();
+            navigate('/login');
+        } catch (error) {
+            console.error("Logout failed", error);
+        }
+    };
+
+
+    const handleSearch = (value: string) => {
+        console.log(value);
+    };
+
+    // Placeholder for navigating to the user profile
+    const goToProfile = () => {
+        navigate('/profile');
+    };
+
     return (
-        <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 50px' }}>
-            <Button type="primary" onClick={handleLogout}>
-                Log out
-            </Button>
+        <Header style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 50px'}}>
+            <Button type="primary" onClick={handleLogout}>Log out</Button>
             <Search
                 placeholder="Search movies"
                 onSearch={handleSearch}
@@ -32,7 +44,7 @@ const AppHeader: React.FC<AppHeaderProps> = ({ handleLogout, handleSearch, goToP
                     width: '600px'
                 }}
             />
-            <Avatar size="large" onClick={goToProfile} icon={<UserOutlined />} src={profileImageUrl} />
+            <Avatar size="large" onClick={goToProfile} icon={<UserOutlined/>} src={profileImageUrl}/>
         </Header>
     );
 };
