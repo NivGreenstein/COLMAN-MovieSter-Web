0983f3c0ec819ce252e109d8787a09d06645acc1
Niv Greenstein <greenstein9@gmail.com>
2024-03-02 17:14:49 +0200

    feat: added /me route

--- BE/src/routes/user.router.ts
+++ BE/src/routes/user.router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { updateUser, getUserById, getUserByEmail } from '../controllers/user.controller';
+import { updateUser, getUserById, getUserByEmail, getCurrentUser } from '../controllers/user.controller';
 
 const router = Router();
 
+router.get('/me', getCurrentUser);
 router.get('/email/:email', getUserByEmail);
 router.get('/:id', getUserById);
 router.patch('/', updateUser);
