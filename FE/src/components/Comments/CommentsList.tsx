8a67f9a7519125f4d20320ac4d43317dc94af848
guylevi <guylevi79@gmail.com>
2024-03-02 19:33:44 +0200

    added edit and delete buttons

--- FE/src/components/Comments/CommentsList.tsx
+++ FE/src/components/Comments/CommentsList.tsx
@@ -1,49 +1,66 @@
-import React from 'react';
-import { Tooltip, List, Rate, Avatar } from 'antd';
-import { Comment as AntComment } from '@ant-design/compatible';
+import React, {useContext, useState} from 'react';
+import { Tooltip, List, Rate, Avatar, Button } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import { Comment } from '../../types/IComment';
+import { IUser } from "../../types/IUser";
+import {SessionContext} from "../../context/SessionContext";
 
 interface CommentListProps {
-  comments: Comment[];
-  isMoviePage: boolean;
+    comments: Comment[];
+    isMoviePage: boolean;
 }
 
 const CommentList: React.FC<CommentListProps> = ({ comments, isMoviePage }) => {
-  return (
-    <div style={{ maxHeight: '60vh', overflowY: 'scroll' }}>
-      <List
-        className="comment-list"
-        itemLayout="horizontal"
-        dataSource={comments}
-        renderItem={(comment: Comment) => (
-          <li>
-            <AntComment
-              author={isMoviePage ? comment.user?.username : comment.movie?.title}
-              avatar={
-                <Avatar
-                  shape={isMoviePage ? 'circle' : 'square'}
-                  src={isMoviePage ? comment.user?.profilePictureUrl : comment.movie?.posterUrl}
-                  alt={isMoviePage ? comment.user?.username : comment.movie?.title}
-                />
-              }
-              content={
-                <>
-                  <Rate disabled value={comment.rating} />
-                  <p>{comment.description}</p>
-                </>
-              }
-              datetime={
-                <Tooltip title={moment(comment.createdAt).format('YYYY-MM-DD HH:mm:ss')}>
-                  <span>{moment(comment.createdAt).fromNow()}</span>
-                </Tooltip>
-              }
+    const { loggedUser } = useContext(SessionContext); // Consuming the context
+
+    const handleEditComment = (commentId: string) => {
+        // Placeholder for edit comment logic
+        console.log(`Edit comment ${commentId}`);
+    };
+
+    const handleDeleteComment = (commentId: string) => {
+        // Placeholder for delete comment logic
+        console.log(`Delete comment ${commentId}`);
+    };
+
+    return (
+        <div style={{ maxHeight: '60vh', overflowY: 'scroll' }}>
+            <List
+                className="comment-list"
+                itemLayout="horizontal"
+                dataSource={comments}
+                renderItem={comment => (
+                    <List.Item
+                        actions={comment.userId === loggedUser?._id ? [
+                            <Button icon={<EditOutlined />} onClick={() => handleEditComment(comment._id)}>Edit</Button>,
+                            <Button icon={<DeleteOutlined />} onClick={() => handleDeleteComment(comment._id)}>Delete</Button>
+                        ] : []}
+                    >
+                        <List.Item.Meta
+                            avatar={
+                                <Avatar
+                                    shape="circle"
+                                    src={comment.user?.profilePictureUrl || comment.movie?.posterUrl}
+                                    alt={comment.user?.username || comment.movie?.title}
+                                />
+                            }
+                            title={isMoviePage ? comment.user?.username : comment.movie?.title}
+                            description={
+                                <>
+                                    <Rate disabled value={comment.rating} />
+                                    <p>{comment.description}</p>
+                                </>
+                            }
+                        />
+                        <Tooltip title={moment(comment.createdAt).format('YYYY-MM-DD HH:mm:ss')}>
+                            <span>{moment(comment.createdAt).fromNow()}</span>
+                        </Tooltip>
+                    </List.Item>
+                )}
             />
-          </li>
-        )}
-      />
-    </div>
-  );
+        </div>
+    );
 };
 
 export default CommentList;
--- FE/src/types/IComment.ts
+++ FE/src/types/IComment.ts
@@ -1,19 +1,3 @@
-// export interface IUserComment {
-//     movieTitle: string;
-//     rating: number;
-//     text: string;
-//     date: string | Date;
-// }
-
-// export interface IMovieComment {
-//     username: string;
-//     rating: number;
-//     text: string;
-//     date: string | Date;
-// }
-
-// export type Comment = IUserComment | IMovieComment;
-
 import { z } from 'zod';
 import { userSchema } from './IUser';
 import { movieSchema } from './IMovie';
