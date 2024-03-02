129bf8ed224aef95ea70a9e381e0f413f0e7a2a7
Niv Greenstein <greenstein9@gmail.com>
2024-03-02 17:51:53 +0200

    fix: added comment full schema

--- FE/src/types/IComment.ts
+++ FE/src/types/IComment.ts
@@ -20,20 +20,24 @@ import { movieSchema } from './IMovie';
 
 export const CommentSchema = z
   .object({
-    _id: z.string().regex(/^[0-9a-fA-F]{24}$/),
     movieId: z.number(),
-    movie: movieSchema.optional(),
     userId: z.string().regex(/^[0-9a-fA-F]{24}$/),
-    user: userSchema.optional(),
     mainCommentId: z
       .string()
       .regex(/^[0-9a-fA-F]{24}$/)
       .optional(),
     description: z.string(),
     rating: z.number().min(0).max(5),
-    createdAt: z.date(),
-    updatedAt: z.date(),
   })
   .strict();
 
-export type Comment = z.infer<typeof CommentSchema>;
+export const CommentFullSchema = CommentSchema.extend({
+  _id: z.string().regex(/^[0-9a-fA-F]{24}$/),
+  movie: movieSchema.optional(),
+  user: userSchema.optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+}).strict();
+
+export type CommentBase = z.infer<typeof CommentSchema>;
+export type Comment = z.infer<typeof CommentFullSchema>;
