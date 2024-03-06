e0ee7e80b975506a8011dfed7388767217d71a51
Niv Greenstein <greenstein9@gmail.com>
2024-03-07 00:48:35 +0200

    feat: added base user schema and type

--- FE/src/types/IUser.ts
+++ FE/src/types/IUser.ts
@@ -1,16 +1,20 @@
 import { z } from 'zod';
 
-export const userSchema = z
-  .object({
+export const baseUserSchema = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  profilePictureUrl: z.string().url(),
+  password: z.string().min(8).optional(),
+  isGoogleUser: z.boolean().default(false).optional(),
+});
+
+export const userSchema = baseUserSchema
+  .extend({
     _id: z.string().regex(/^[0-9a-fA-F]{24}$/),
-    username: z.string(),
-    email: z.string().email(),
-    profilePictureUrl: z.string().url(),
-    password: z.string().min(8).optional(),
-    isGoogleUser: z.boolean().default(false).optional(),
     createdAt: z.date(),
     updatedAt: z.date(),
   })
   .strict();
 
 export type IUser = z.infer<typeof userSchema>;
+export type IBaseUser = z.infer<typeof baseUserSchema>;
