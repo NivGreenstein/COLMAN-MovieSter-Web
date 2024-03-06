4761cc696a61b34919bda4be70b53425a24575cf
Niv Greenstein <greenstein9@gmail.com>
2024-03-07 00:22:33 +0200

    feat: added https on node_env production

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { config as dotenvConfig } from 'dotenv';
 import helmet from 'helmet';
+import https from 'https';
+import fs from 'fs';
 //@ts-ignore
 import { xss } from 'express-xss-sanitizer';
 import cookieParser from 'cookie-parser';
@@ -10,7 +12,7 @@ import { closeMongoDbConnection, getMongoDbClient } from './DB/monogDb';
 
 dotenvConfig();
 
-const { PORT: serverPort } = process.env;
+const { PORT: serverPort, NODE_ENV } = process.env;
 const app = express();
 const corsOptions: CorsOptions = {
   origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5173'],
@@ -18,14 +20,30 @@ const corsOptions: CorsOptions = {
   optionsSuccessStatus: 200,
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
 app.use(express.json({}), helmet(), xss(), cors(corsOptions), cookieParser());
 app.use('/', mainRouter);
 
-app.listen(serverPort, async () => {
-  await getMongoDbClient();
-  console.log(`server is listening on port ${serverPort}`);
-});
+if (NODE_ENV === 'production') {
+  const privateKey = fs.readFileSync('../cert/client-key.pem');
+  const certificate = fs.readFileSync('../cert/client-cert.pem');
+
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+  };
+
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(serverPort, async () => {
+    await getMongoDbClient();
+    console.log(`HTTPS Server running on port ${serverPort}`);
+  });
+} else {
+  app.listen(serverPort, async () => {
+    await getMongoDbClient();
+    console.log(`HTTP Server running on port ${serverPort}`);
+  });
+}
 
 const shutdown = async () => {
   console.log('Gracefully shutting down');
