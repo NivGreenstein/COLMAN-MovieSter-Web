69c31de93243ac85b17842b057dc57e3eda2f781
guylevi <guylevi79@gmail.com>
2024-03-13 01:05:11 +0200

    fixed profile picture bug and thread bug

--- BE/src/services/user.service.ts
+++ BE/src/services/user.service.ts
@@ -1,111 +1,114 @@
-import { InsertOneResult, ObjectId, WithId } from 'mongodb';
+import {InsertOneResult, ObjectId, WithId} from 'mongodb';
 import bcrypt from 'bcrypt';
-import { getDb } from '../DB/monogDb';
-import { UserMongoDB, UserRegister } from '../models/user.model';
-import { PartialUserUpdate } from '../controllers/user.controller';
-import { generateAccessToken, generateRefreshToken } from '../utils/jwt.util';
+import {getDb} from '../DB/monogDb';
+import {UserMongoDB, UserRegister} from '../models/user.model';
+import {PartialUserUpdate} from '../controllers/user.controller';
+import {generateAccessToken, generateRefreshToken} from '../utils/jwt.util';
 
 const getCollection = async () => {
-  const db = await getDb();
-  return db.collection<UserMongoDB>('users');
+    const db = await getDb();
+    return db.collection<UserMongoDB>('users');
 };
 
 export const updateUser = async (user: PartialUserUpdate) => {
-  const collection = await getCollection();
-  const updatedUser = { ...user, _id: undefined, updatedAt: new Date() };
-  delete updatedUser._id;
-  return await collection.updateOne({ _id: new ObjectId(user._id) }, { $set: { ...updatedUser } });
+    const collection = await getCollection();
+    const updatedUser = {...user, _id: undefined, updatedAt: new Date()};
+    delete updatedUser._id;
+    if (user.profilePictureUrl) {
+        updatedUser.profilePictureUrl = user.profilePictureUrl;
+    }
+    return await collection.updateOne({_id: new ObjectId(user._id)}, {$set: {...updatedUser}});
 };
 
 export const getUserById = async (id: string): Promise<WithId<UserMongoDB> | null> => {
-  const collection = await getCollection();
-  const user = await collection.findOne({ _id: new ObjectId(id) }, { projection: { password: 0 } });
-  return user;
+    const collection = await getCollection();
+    const user = await collection.findOne({_id: new ObjectId(id)}, {projection: {password: 0}});
+    return user;
 };
 
 export const getUserByEmail = async (email: string) => {
-  const collection = await getCollection();
-  return await collection.findOne({ email }, { projection: { password: 0 } });
+    const collection = await getCollection();
+    return await collection.findOne({email}, {projection: {password: 0}});
 };
 
 export const createUser = async (user: UserRegister) => {
-  const collection = await getCollection();
+    const collection = await getCollection();
 
-  const exsits = await collection.findOne(
-    {
-      $or: [{ username: user.username }, { email: user.email }],
-    },
-    { projection: { password: 0 } }
-  );
+    const exsits = await collection.findOne(
+        {
+            $or: [{username: user.username}, {email: user.email}],
+        },
+        {projection: {password: 0}}
+    );
 
-  if (exsits) {
-    throw new Error('User already exists');
-  }
-  const hashedPassword = bcrypt.hashSync(user.password, 10);
+    if (exsits) {
+        throw new Error('User already exists');
+    }
+    const hashedPassword = bcrypt.hashSync(user.password, 10);
 
-  const result = await collection.insertOne({
-    ...user,
-    password: hashedPassword,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
+    const result = await collection.insertOne({
+        ...user,
+        password: hashedPassword,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
 
-  if (!result.acknowledged) {
-    throw new Error('Could not create user');
-  }
-  return result.insertedId;
+    if (!result.acknowledged) {
+        throw new Error('Could not create user');
+    }
+    return result.insertedId;
 };
 
 export const login = async (email: string, password: string) => {
-  const collection = await getCollection();
+    const collection = await getCollection();
 
-  const user = await collection.findOne({ email });
+    const user = await collection.findOne({email});
 
-  if (!user) {
-    throw new Error('User not found');
-  }
+    if (!user) {
+        throw new Error('User not found');
+    }
 
-  const isPasswordCorrect = bcrypt.compareSync(password, user.password!!);
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password!!);
 
-  if (!isPasswordCorrect) {
-    throw new Error('Password is incorrect');
-  }
+    if (!isPasswordCorrect) {
+        throw new Error('Password is incorrect');
+    }
 
-  const { password: _, ...userWithoutPassword } = user;
-  const accessToken: string = generateAccessToken({ ...userWithoutPassword });
-  const refreshToken: string = await generateRefreshToken({ ...userWithoutPassword });
+    const {password: _, ...userWithoutPassword} = user;
+    const accessToken: string = generateAccessToken({...userWithoutPassword});
+    const refreshToken: string = await generateRefreshToken({...userWithoutPassword});
 
-  return { accessToken, refreshToken };
+    return {accessToken, refreshToken};
 };
 
 export const googleLogin = async (email: string, name: string, profilePictureUrl: string) => {
-  const collection = await getCollection();
+    const collection = await getCollection();
 
-  let user: WithId<UserMongoDB> | null = await collection.findOne({ email });
+    let user: WithId<UserMongoDB> | null = await collection.findOne({email});
 
-  if (!user) {
-    const insertion = await collection.insertOne({
-      email,
-      username: name,
-      profilePictureUrl,
-      isGoogleUser: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    if (insertion.acknowledged) {
-      user = await collection.findOne({ email });
-    }
     if (!user) {
-      throw new Error('Could not create user');
+        const insertion = await collection.insertOne({
+            email,
+            username: name,
+            profilePictureUrl,
+            isGoogleUser: true,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+        if (insertion.acknowledged) {
+            user = await collection.findOne({email});
+        }
+        if (!user) {
+            throw new Error('Could not create user');
+        }
     }
-  }
-  const accessToken: string = generateAccessToken(user);
-  const refreshToken: string = await generateRefreshToken(user);
+    const accessToken: string = generateAccessToken(user);
+    const refreshToken: string = await generateRefreshToken(user);
 
-  return { accessToken, refreshToken };
+    return {accessToken, refreshToken};
 };
 
 export const deleteUser = async (id: string) => {
-  const collection = await getCollection();
-  return await collection.deleteOne({ _id: new ObjectId(id) });
+    const collection = await getCollection();
+    return await collection.deleteOne({_id: new ObjectId(id)});
 };
--- FE/src/components/Comments/CommentsList.tsx
+++ FE/src/components/Comments/CommentsList.tsx
@@ -1,190 +1,194 @@
-import React, { useState } from 'react';
-import { Image, Tooltip, List, Rate, Avatar, Button, Typography } from 'antd';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Image, Tooltip, List, Rate, Avatar, Button, Typography} from 'antd';
+import {EditOutlined, DeleteOutlined} from '@ant-design/icons';
 import moment from 'moment';
-import { Comment, CommentFullSchema } from '../../types/IComment';
-import { useSession } from '../../context/SessionContext';
-import { deleteComment, patchComment } from '../../services/comments.service';
+import {Comment, CommentFullSchema} from '../../types/IComment';
+import {useSession} from '../../context/SessionContext';
+import {deleteComment, patchComment} from '../../services/comments.service';
 import AddCommentDialog from './AddCommentDialog';
-import { CommentThreadModal } from './CommentThreadModal';
-import { RcFile } from 'antd/es/upload/interface';
-import { useNavigate } from 'react-router-dom';
+import {CommentThreadModal} from './CommentThreadModal';
+import {RcFile} from 'antd/es/upload/interface';
+import {useNavigate} from 'react-router-dom';
 
 interface CommentListProps {
-  comments: Comment[];
-  setComments: (comments: Comment[]) => void;
-  isMoviePage: boolean;
-  setImagePreview: (value: string) => void;
-  imagePreview: string | undefined;
-  isCommentThread?: boolean;
+    comments: Comment[];
+    setComments: (comments: Comment[]) => void;
+    isMoviePage: boolean;
+    setImagePreview: (value: string) => void;
+    imagePreview: string | undefined;
+    isCommentThread?: boolean;
 }
 
 const CommentList: React.FC<CommentListProps> = ({
-  comments,
-  isMoviePage,
-  setComments,
-  imagePreview,
-  setImagePreview,
-  isCommentThread = false,
-}) => {
-  const navigate = useNavigate();
-  const [isAddCommentModalVisible, setIsAddCommentModalVisible] = useState(false);
-  const [activeThreadCommentId, setActiveThreadCommentId] = useState<string | null>(null);
+                                                     comments,
+                                                     isMoviePage,
+                                                     setComments,
+                                                     imagePreview,
+                                                     setImagePreview,
+                                                     isCommentThread = false,
+                                                 }) => {
+    const navigate = useNavigate();
+    const [isAddCommentModalVisible, setIsAddCommentModalVisible] = useState(false);
+    const [activeThreadCommentId, setActiveThreadCommentId] = useState<string | null>(null);
 
-  const [commentIdToEdit, setCommentIdToEdit] = useState('');
-  const [description, setDescription] = useState('');
-  const [rating, setRating] = useState(0);
-  const { loggedUser } = useSession();
-  const [image, setImage] = useState<RcFile | null>(null);
+    const [commentIdToEdit, setCommentIdToEdit] = useState('');
+    const [description, setDescription] = useState('');
+    const [rating, setRating] = useState(0);
+    const {loggedUser} = useSession();
+    const [image, setImage] = useState<RcFile | null>(null);
 
-  const handleDeleteComment = async (commentId: string) => {
-    const response = await deleteComment(commentId);
-    if (response?.ok) {
-      console.log('Comment deleted', response);
-      const updatedComments = comments.filter((comment) => comment._id !== commentId);
-      setComments(updatedComments);
-    }
-  };
-
-  const handleEditButtonClick = (commentId: string) => {
-    setCommentIdToEdit(commentId);
-    const comment = comments.find((comment) => comment._id === commentId);
-    if (!comment) throw new Error('No comment found');
-    setDescription(comment.description);
-    setRating(comment.rating);
-    setIsAddCommentModalVisible(true);
-    if (comment.imagePath) {
-      setImagePreview(`${import.meta.env.VITE_API_URI}/${comment.imagePath}`);
-    }
-  };
-
-  const handleEdit = async () => {
-    if (!commentIdToEdit) throw new Error('No comment to edit');
-
-    const commentToUpdate = {
-      _id: commentIdToEdit,
-      description: description,
-      rating: rating,
-      imagePath: imagePreview,
+    const handleDeleteComment = async (commentId: string) => {
+        const response = await deleteComment(commentId);
+        if (response?.ok) {
+            console.log('Comment deleted', response);
+            const updatedComments = comments.filter((comment) => comment._id !== commentId);
+            setComments(updatedComments);
+        }
     };
-    CommentFullSchema.partial().parse(commentToUpdate);
-    try {
-      const response = await patchComment(commentToUpdate, image ? (image as File) : undefined);
 
-      console.log('Comment updated', response);
-      const updatedComments = comments.map((comment) =>
-        comment._id === commentIdToEdit
-          ? {
-              ...comment,
-              ...commentToUpdate,
-              imagePath: response.imagePath,
-            }
-          : comment,
-      );
-      setComments(updatedComments);
-      setIsAddCommentModalVisible(false);
-      setCommentIdToEdit('');
-    } catch (error) {
-      console.error('Error updating comment', error);
-    }
-  };
-  return (
-    <>
-      <div style={{ maxHeight: '60vh', overflowY: 'scroll' }}>
-        <List
-          className="comment-list"
-          itemLayout="horizontal"
-          dataSource={comments}
-          renderItem={(comment) => (
-            <>
-              <List.Item
-                actions={
-                  comment.userId === loggedUser?._id
-                    ? [
-                        <Button icon={<EditOutlined />} onClick={() => handleEditButtonClick(comment._id)}>
-                          Edit
-                        </Button>,
-                        <Button icon={<DeleteOutlined />} onClick={() => handleDeleteComment(comment._id)}>
-                          Delete
-                        </Button>,
-                      ]
-                    : []
-                }
-              >
-                <List.Item.Meta
-                  avatar={
-                    <Avatar
-                      onClick={() => {
-                        if (isMoviePage) {
-                          navigate(`/profile/${comment.user?._id}`);
-                        } else {
-                          navigate(`/movie/${comment.movieId}`);
-                        }
-                      }}
-                      style={{ cursor: 'pointer' }}
-                      shape="circle"
-                      src={comment.user?.profilePictureUrl ?? comment.movie?.posterUrl}
-                    />
-                  }
-                  title={isMoviePage ? comment.user?.username : comment.movie?.title}
-                  description={
-                    <>
-                      {!isCommentThread && <Rate disabled value={comment.rating} />}
-                      <Typography.Paragraph ellipsis={{ rows: 2, expandable: true, symbol: 'more' }}>
-                        {comment.description}
-                      </Typography.Paragraph>
-                      {!isCommentThread && comment.imagePath && (
-                        <Image
-                          width={50}
-                          src={`${import.meta.env.VITE_API_URI}/${comment.imagePath}`}
-                          alt="Comment image"
-                          style={{ display: 'block', marginTop: '10px' }}
-                        />
-                      )}
-                    </>
-                  }
+    const handleEditButtonClick = (commentId: string) => {
+        setCommentIdToEdit(commentId);
+        const comment = comments.find((comment) => comment._id === commentId);
+        if (!comment) throw new Error('No comment found');
+        setDescription(comment.description);
+        setRating(comment.rating);
+        setIsAddCommentModalVisible(true);
+        if (comment.imagePath) {
+            setImagePreview(`${import.meta.env.VITE_API_URI}/${comment.imagePath}`);
+        }
+    };
+
+    const handleEdit = async () => {
+        if (!commentIdToEdit) throw new Error('No comment to edit');
+
+        const commentToUpdate = {
+            _id: commentIdToEdit,
+            description: description,
+            rating: rating,
+            imagePath: imagePreview,
+        };
+        CommentFullSchema.partial().parse(commentToUpdate);
+        try {
+            const response = await patchComment(commentToUpdate, image ? (image as File) : undefined);
+
+            console.log('Comment updated', response);
+            const updatedComments = comments.map((comment) =>
+                comment._id === commentIdToEdit
+                    ? {
+                        ...comment,
+                        ...commentToUpdate,
+                        imagePath: response.imagePath,
+                    }
+                    : comment,
+            );
+            setComments(updatedComments);
+            setIsAddCommentModalVisible(false);
+            setCommentIdToEdit('');
+        } catch (error) {
+            console.error('Error updating comment', error);
+        }
+    };
+    return (
+        <>
+            <div style={{maxHeight: '60vh', overflowY: 'scroll'}}>
+                <List
+                    className="comment-list"
+                    itemLayout="horizontal"
+                    dataSource={comments}
+                    renderItem={(comment) => (
+                        <>
+                            <List.Item
+                                actions={
+                                    comment.userId === loggedUser?._id
+                                        ? [
+                                            <Button icon={<EditOutlined/>}
+                                                    onClick={() => handleEditButtonClick(comment._id)}>
+                                                Edit
+                                            </Button>,
+                                            <Button icon={<DeleteOutlined/>}
+                                                    onClick={() => handleDeleteComment(comment._id)}>
+                                                Delete
+                                            </Button>,
+                                        ]
+                                        : []
+                                }
+                            >
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar
+                                            onClick={() => {
+                                                if (isMoviePage) {
+                                                    navigate(`/profile/${comment.user?._id}`);
+                                                } else {
+                                                    navigate(`/movie/${comment.movieId}`);
+                                                }
+                                            }}
+                                            style={{cursor: 'pointer'}}
+                                            shape="circle"
+                                            src={`${import.meta.env.VITE_API_URI}/${comment.user?.profilePictureUrl}` ?? comment.movie?.posterUrl}
+                                        />
+                                    }
+                                    title={isMoviePage ? comment.user?.username : comment.movie?.title}
+                                    description={
+                                        <>
+                                            { <Rate disabled value={comment.rating}/>}
+                                            <Typography.Paragraph
+                                                ellipsis={{rows: 2, expandable: true, symbol: 'more'}}>
+                                                {comment.description}
+                                            </Typography.Paragraph>
+                                            {comment.imagePath && (
+                                                <Image
+                                                    width={50}
+                                                    src={`${import.meta.env.VITE_API_URI}/${comment.imagePath}`}
+                                                    alt="Comment image"
+                                                    style={{display: 'block', marginTop: '10px'}}
+                                                />
+                                            )}
+                                        </>
+                                    }
+                                />
+                                <Tooltip title={moment(comment.createdAt).format('YYYY-MM-DD HH:mm:ss')}>
+                                    <span>{moment(comment.createdAt).fromNow()}</span>
+                                </Tooltip>
+                                {!isCommentThread && (
+                                    <div>
+                                        <Button onClick={() => setActiveThreadCommentId(comment._id)}>See
+                                            thread</Button>
+                                        <CommentThreadModal
+                                            isModalVisible={activeThreadCommentId === comment._id}
+                                            setIsModalVisible={() => setActiveThreadCommentId(null)}
+                                            comment={comment}
+                                            setImage={setImage}
+                                            setImagePreview={setImagePreview}
+                                            imagePreview={imagePreview}
+                                            image={image}
+                                        />
+                                    </div>
+                                )}
+                            </List.Item>
+                        </>
+                    )}
                 />
-                <Tooltip title={moment(comment.createdAt).format('YYYY-MM-DD HH:mm:ss')}>
-                  <span>{moment(comment.createdAt).fromNow()}</span>
-                </Tooltip>
-                {!isCommentThread && (
-                  <div>
-                    <Button onClick={() => setActiveThreadCommentId(comment._id)}>See thread</Button>
-                    <CommentThreadModal
-                      isModalVisible={activeThreadCommentId === comment._id}
-                      setIsModalVisible={() => setActiveThreadCommentId(null)}
-                      comment={comment}
-                      setImage={setImage}
-                      setImagePreview={setImagePreview}
-                      imagePreview={imagePreview}
-                      image={image}
-                    />
-                  </div>
-                )}
-              </List.Item>
-            </>
-          )}
-        />
-      </div>
-      <AddCommentDialog
-        isModalVisible={isAddCommentModalVisible}
-        setIsModalVisible={setIsAddCommentModalVisible}
-        handleSubmit={handleEdit}
-        rating={rating}
-        setRating={setRating}
-        description={description}
-        setDescription={setDescription}
-        isEditMode={true}
-        setImage={setImage}
-        setImagePreview={setImagePreview}
-        imagePreview={imagePreview}
-        restartStates={() => {
-          setDescription('');
-          setRating(0);
-        }}
-      />
-    </>
-  );
+            </div>
+            <AddCommentDialog
+                isModalVisible={isAddCommentModalVisible}
+                setIsModalVisible={setIsAddCommentModalVisible}
+                handleSubmit={handleEdit}
+                rating={rating}
+                setRating={setRating}
+                description={description}
+                setDescription={setDescription}
+                isEditMode={true}
+                setImage={setImage}
+                setImagePreview={setImagePreview}
+                imagePreview={imagePreview}
+                restartStates={() => {
+                    setDescription('');
+                    setRating(0);
+                }}
+            />
+        </>
+    );
 };
 
 export default CommentList;
